0e741486c3f84cb9fff31f9742e18906bb0ec0e6
Muhammad Alfian Dwi Fantara <alfiandwi48@gmail.com>
2022-09-05 22:40:55 +0700

    ❤️ init!

---
+++ components/FeaturedPost.js
@@ -0,0 +1,30 @@
+import InfoPost from "@/components/InfoPost";
+import Link from "next/link";
+
+export default function FeaturedPost() {
+  return (
+    <article>
+      <div className="flex -mx-4 lg:items-center items-start flex-wrap">
+        <div className="px-4 lg:w-8/12 md:7/12 w-full">
+          <Link href="/detail">
+            <a>
+              <img src="/featured-thumbnail.png" className="rounded-xl" />
+            </a>
+          </Link>
+        </div>
+        <div className="px-4 lg:w-4/12 md:5/12 w-full">
+          <InfoPost 
+            category="UI DESIGN"
+            date="August 2, 2022"
+            title="Understanding color theory: the color wheel and finding complementary colors"
+            shortDescription="Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum."
+            authorAvatar="/author-1.png"
+            authorName="Leslie Alexander"
+            authorJob="UI Designer"
+          />
+        </div>
+      </div>
+      <hr className="border-white/10 mt-8 lg:hidden" />
+    </article>
+  );
+}
---
+++ components/PostAuthor.js
@@ -0,0 +1,11 @@
+export default function PostAuthor({ authorAvatar, authorJob, authorName }) {
+  return (
+    <div className="flex items-center mt-6">
+      <img src={authorAvatar} className="w-14 h-14 rounded-full object-cover mt-4" />
+      <div className="ml-4">
+        <h3>{authorName}</h3>
+        <div className="text-white/60 text-sm mt-1">{authorJob}</div>
+      </div>
+    </div>
+  );
+}
